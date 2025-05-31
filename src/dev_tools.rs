ba2ab85d9bb898b5a322796393b61ea9d9dcedfa
Austin Almond <aust.almond@gmail.com>
2025-05-31 17:44:37 -0400

    Fix bevy-inspector-egui

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,14 +1,19 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
+use crate::screens::Screen;
 use bevy::{
     dev_tools::states::log_transitions, input::common_conditions::input_just_pressed, prelude::*,
     ui::UiDebugOptions,
 };
-
-use crate::screens::Screen;
+use bevy_inspector_egui::bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins(bevy_inspector_egui::DefaultInspectorConfigPlugin);
+    // bevy-inspector-egui
+    app.add_plugins(EguiPlugin {
+        enable_multipass_for_primary_context: true,
+    })
+    .add_plugins(WorldInspectorPlugin::new());
 
     // Log `Screen` state transitions.
     app.add_systems(Update, log_transitions::<Screen>);
