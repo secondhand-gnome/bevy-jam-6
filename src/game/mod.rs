973205743929125a2b8b82ede351df461865e571
Austin Almond <aust.almond@gmail.com>
2025-05-31 17:19:34 -0400

    Create game create instead of using demo crate directly

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,10 +1,13 @@
+pub mod level;
+pub mod player;
+
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((
         // crate::demo::animation::plugin,
-        // level::plugin,
+        level::plugin,
         // crate::demo::movement::plugin,
-        // player::plugin,
+        player::plugin,
     ));
 }
---
+++ src/game/player.rs
@@ -0,0 +1,71 @@
+//! Player-specific behavior.
+
+use bevy::{
+    image::{ImageLoaderSettings, ImageSampler},
+    prelude::*,
+};
+
+use crate::{AppSystems, PausableSystems, asset_tracking::LoadResource};
+
+pub(super) fn plugin(app: &mut App) {
+    app.register_type::<Player>();
+
+    app.register_type::<PlayerAssets>();
+    app.load_resource::<PlayerAssets>();
+
+    // TODO listen to input
+}
+
+/// The player character.
+// TODO spawn from Level
+pub fn player(
+    max_speed: f32,
+    player_assets: &PlayerAssets,
+    texture_atlas_layouts: &mut Assets<TextureAtlasLayout>,
+) -> impl Bundle {
+    // A texture atlas is a way to split a single image into a grid of related images.
+    // You can learn more in this example: https://github.com/bevyengine/bevy/blob/latest/examples/2d/texture_atlas.rs
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(64), 3, 1, Some(UVec2::splat(1)), None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    // let player_animation = PlayerAnimation::new();
+
+    (
+        Name::new("Player"),
+        Player,
+        Sprite {
+            image: player_assets.farmer.clone(),
+            texture_atlas: Some(TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 0, // TODO change index based on player moving left/right
+            }),
+            ..default()
+        },
+        Transform::from_scale(Vec2::splat(8.0).extend(1.0)),
+    )
+}
+
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Default, Reflect)]
+#[reflect(Component)]
+struct Player;
+
+#[derive(Resource, Asset, Clone, Reflect)]
+#[reflect(Resource)]
+pub struct PlayerAssets {
+    #[dependency]
+    farmer: Handle<Image>,
+}
+
+impl FromWorld for PlayerAssets {
+    fn from_world(world: &mut World) -> Self {
+        let assets = world.resource::<AssetServer>();
+        Self {
+            farmer: assets.load_with_settings(
+                "images/farmer.png",
+                |settings: &mut ImageLoaderSettings| {
+                    // Use `nearest` image sampling to preserve pixel art style.
+                    settings.sampler = ImageSampler::nearest();
+                },
+            ),
+        }
+    }
+}
