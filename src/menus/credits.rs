9c93499b4dd2f070b70e78f02d1812fb18f9b9ac
Austin Almond <aust.almond@gmail.com>
2025-06-08 22:38:35 -0400

    Remove credits music

--- src/menus/credits.rs
+++ src/menus/credits.rs
@@ -4,7 +4,7 @@ use bevy::{
     ecs::spawn::SpawnIter, input::common_conditions::input_just_pressed, prelude::*, ui::Val::*,
 };
 
-use crate::{asset_tracking::LoadResource, audio::music, menus::Menu, theme::prelude::*};
+use crate::{menus::Menu, theme::prelude::*};
 
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Menu::Credits), spawn_credits_menu);
@@ -13,8 +13,6 @@ pub(super) fn plugin(app: &mut App) {
         go_back.run_if(in_state(Menu::Credits).and(input_just_pressed(KeyCode::Escape))),
     );
 
-    app.register_type::<CreditsAssets>();
-    app.load_resource::<CreditsAssets>();
     app.add_systems(OnEnter(Menu::Credits), start_credits_music);
 }
 
@@ -48,8 +46,6 @@ fn assets() -> impl Bundle {
         ["Lots of sprites", "CC0 by Kenney"],
         ["Pineapple", "CC0 by Master484"],
         ["Button SFX", "CC0 by Jaszunio15"],
-        ["Music", "CC BY 3.0 by Kevin MacLeod"],
-        ["Music", "CC BY 3.0 by Kevin MacLeod"],
         ["Banjo Romp Music", "Charles Shomo from Pixabay"],
         [
             "Bevy logo",
@@ -94,26 +90,6 @@ fn go_back(mut next_menu: ResMut<NextState<Menu>>) {
     next_menu.set(Menu::Main);
 }
 
-#[derive(Resource, Asset, Clone, Reflect)]
-#[reflect(Resource)]
-struct CreditsAssets {
-    #[dependency]
-    music: Handle<AudioSource>,
-}
-
-impl FromWorld for CreditsAssets {
-    fn from_world(world: &mut World) -> Self {
-        let assets = world.resource::<AssetServer>();
-        Self {
-            music: assets.load("audio/music/Monkeys Spinning Monkeys.ogg"),
-        }
-    }
-}
-
-fn start_credits_music(mut commands: Commands, credits_music: Res<CreditsAssets>) {
-    commands.spawn((
-        Name::new("Credits Music"),
-        StateScoped(Menu::Credits),
-        music(credits_music.music.clone()),
-    ));
+fn start_credits_music(mut commands: Commands) {
+    commands.spawn((Name::new("Credits Music"), StateScoped(Menu::Credits)));
 }
